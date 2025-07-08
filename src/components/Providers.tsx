30d1de18717d640faeb4cde1ca7fe74f42785496
James Bowers <jamesbowers123@gmail.com>
2025-07-08 11:59:47 +0100

    initialise NextJS project with Tailwind V3

---
+++ src/components/Providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { HeroUIProvider } from "@heroui/react"
+import React from "react"
+
+export default function providers({children}: {children: React.ReactNode}) {
+  return (
+    <HeroUIProvider>
+      {children}
+    </HeroUIProvider>
+  )
+}
