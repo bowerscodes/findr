aeb00054c828e63a925d5a1d16086e85a520a7b6
James Bowers <jamesbowers123@gmail.com>
2025-07-09 13:37:15 +0100

    set up Error handling

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -8,16 +8,29 @@ import { Button, Card, CardBody, CardHeader, Input } from '@heroui/react';
 import { GiPadlock } from 'react-icons/gi';
 
 export default function RegisterForm() {
-const {register, handleSubmit, formState: {errors, isValid}} = useForm<RegisterSchema>({
-    // resolver: zodResolver(registerSchema),
-    mode: "onTouched"
-  });
-  
-  const onSubmit = async (data: RegisterSchema) => {
-    const result = await registerUser(data);
-    console.log(result);
-  };
-  
+  const {register, handleSubmit, setError, formState: { errors, isValid, isSubmitting }} = useForm<RegisterSchema>({
+      // resolver: zodResolver(registerSchema),
+      mode: "onTouched"
+    });
+
+    const onSubmit = async (data: RegisterSchema) => {
+      const result = await registerUser(data);
+
+      if (result.status === "success") {
+        console.log("User registered successfully")
+      } else {
+        if (Array.isArray(result.error)) {
+          result.error.forEach((e) => {
+            const fieldName = e.path.join(".") as 'email' | 'name' | 'password';
+            setError(fieldName, { message: e.message })
+          })
+      } else {
+        setError("root.serverError", { message: result.error });
+      }
+
+    };
+  }
+
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
@@ -57,7 +70,16 @@ const {register, handleSubmit, formState: {errors, isValid}} = useForm<RegisterS
               isInvalid={!!errors.password}
               errorMessage={errors.password?.message}
             />
-            <Button isDisabled={!isValid} fullWidth color="secondary" type="submit">
+            {errors.root?.serverError && (
+              <p className="text-danger text-sm">{errors.root.serverError.message}</p>
+            )}
+            <Button 
+              isLoading={isSubmitting}
+              isDisabled={!isValid} 
+              fullWidth 
+              color="secondary"
+              type="submit"
+            >
               Register
             </Button>
           </div>
