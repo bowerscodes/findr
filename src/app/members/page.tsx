141dacdf8f78960c0b4fc0d0e3a33c49dee7d7dc
James Bowers <jamesbowers123@gmail.com>
2025-07-10 12:11:31 +0100

    set up functionality for fetching users from the database

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,12 +1,16 @@
-"use client";
-import { Link } from "@heroui/react";
 import React from "react";
+import { getMembers } from "../actions/memberActions";
 
-export default function MembersPage() {
+export default async function MembersPage() {
+  const members = await getMembers();
+
   return (
     <div>
-      <h3 className="text-3xl">This will be the Members page</h3>
-      <Link href="/">Go back to Home</Link>
+      <ul>
+        {members && members.map(member => (
+          <li key={member.id}>{member.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
