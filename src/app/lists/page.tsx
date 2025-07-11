28f3622600b4d576bbdfdf52546a6fe1e6243826
James Bowers <jamesbowers123@gmail.com>
2025-07-11 14:51:33 +0100

    add list tabs and add LoadingComponent

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function ListsPage() {
+import ListsTab from "./ListsTab";
+import { fetchCurrentUserLikeIds, fetchLikedMembers } from "../actions/likeActions";
+
+export default async function ListsPage({
+  searchParams
+}: { 
+  searchParams: Promise<{ type: string }>
+}) {
+  const { type } = await searchParams;
+
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = await fetchLikedMembers(type);
+
   return (
     <div>
-      ListsPage
+      <ListsTab members={members} likeIds={likeIds} />
     </div>
   )
-}
+};
