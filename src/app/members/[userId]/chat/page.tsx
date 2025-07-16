836f292e8287c7a359d62fd5374bab6e0f26faa2
James Bowers <jamesbowers123@gmail.com>
2025-07-16 15:30:46 +0100

    implement getMessageThread function

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PageClient from "./PageClient";
 import { getMemberById } from "@/app/actions/memberActions";
+import { getMessageThread } from "@/app/actions/messageActions";
 
 
 export default async function ChatPage({ params }: { params: Promise<{ userId: string }> }) {
   const { userId } = await params;
   const member = await getMemberById(userId);
+  const messages = await getMessageThread(userId);
+
+  console.log({ messages });
 
   return (
     <PageClient member={member} />
