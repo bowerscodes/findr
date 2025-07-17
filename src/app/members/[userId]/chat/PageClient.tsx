625927076ff119a9147d477fafe4750f89aa3116
James Bowers <jamesbowers123@gmail.com>
2025-07-17 09:29:01 +0100

    refactor chat page and remove variables which are no longer required

--- src/app/members/[userId]/chat/PageClient.tsx
+++ src/app/members/[userId]/chat/PageClient.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import React from "react";
-import { Member } from "@/generated/prisma";
 import CardInnerWrapper from "@/components/CardInnerWrapper";
 import ChatForm from "./ChatForm";
 import { MessageThread } from "@/types";
 import MessageBox from "./MessageBox";
 
 interface Props {
-  member: Member;
   messages: MessageThread;
   userId: string;
 }
 
-export default function PageClient({ member, messages, userId }: Props) {
+export default function PageClient({ messages, userId }: Props) {
     const body = (
     <div>
       {messages.length === 0 ? "No messages to display" : (
--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import PageClient from "./PageClient";
-import { getMemberById } from "@/app/actions/memberActions";
 import { getMessageThread } from "@/app/actions/messageActions";
 import { MessageThread } from "@/types";
-import { getAuthUserId } from "@/app/actions/authActions";
 
 
 export default async function ChatPage({ params }: { params: Promise<{ userId: string }> }) {
   const { userId } = await params;
-  const member = await getMemberById(userId);
   const messages: MessageThread = await getMessageThread(userId);
 
   console.log({ messages });
 
   return (
-    <PageClient member={member} messages={messages} userId={userId} />
+    <PageClient messages={messages} userId={userId} />
   )
 };
