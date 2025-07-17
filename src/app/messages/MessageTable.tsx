aef556d06f52a3066b372938871078300f32085a
James Bowers <jamesbowers123@gmail.com>
2025-07-17 11:58:26 +0100

    add custom cells to messages table

--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -2,9 +2,11 @@
 
 import { MessageDto } from "@/types";
 import { Card } from "@heroui/card";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@heroui/table";
+import { Avatar, Button } from "@heroui/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@heroui/table";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { Key } from "react";
+import React, { Key, useCallback } from "react";
+import { AiFillDelete } from "react-icons/ai";
 
 type Props = {
   messages: MessageDto[];
@@ -18,7 +20,8 @@ export default function MessageTable({ messages }: Props) {
   const columns = [
     { key: isOutbox ? "recipientName" : "senderName", label: isOutbox ? "Recipient" : "Sender" },
     { key: "text", label: "Message" },
-    { key: "created", label: isOutbox ? "Date sent" : "Date received" }
+    { key: "created", label: isOutbox ? "Date sent" : "Date received" },
+    { key: "actions", label: "Actions" }
   ];
 
   const handleRowSelect = (key: Key) => {
@@ -27,6 +30,38 @@ export default function MessageTable({ messages }: Props) {
       `/members/${message?.recipientId}` : `/members/${message?.senderId}`;
       router.push(url + "/chat");
   };
+
+  const renderCell = useCallback((item: MessageDto, columnKey: keyof MessageDto) => {
+    const cellValue = item[columnKey];
+    
+    switch (columnKey) {
+      case "recipientName":
+      case "senderName": 
+        return (
+          <div className={"flex items-center gap-2 cursor-pointer"}>
+            <Avatar 
+              alt="Image of member"
+              src={(isOutbox ? item.recipientImage : item.senderImage) || "/images/user.png"}
+            />
+            <span>{cellValue}</span>
+          </div>
+        )
+      case "text":
+        return (
+          <div>
+            {cellValue}
+          </div>
+        )
+      case "created":
+        return cellValue
+      default:
+        return (
+          <Button isIconOnly variant="light">
+            <AiFillDelete size={24} className="text-danger" />
+          </Button>
+        )
+    }
+  }, [isOutbox]);
 
   return (
     <Card className="flex flex-col gap-3 h-[80vh] overflow-auto">
@@ -45,10 +80,8 @@ export default function MessageTable({ messages }: Props) {
           {(item) =>
             <TableRow key={item.id} className="cursor-pointer">
               {(columnKey) => 
-                <TableCell>
-                  <div className={`${!item.dateRead && !isOutbox ? "font-semibold" : ""}`}>
-                    {getKeyValue(item, columnKey)}
-                  </div>
+                <TableCell className={`${!item.dateRead && !isOutbox ? "font-semibold" : ""}`}>
+                  {renderCell(item, columnKey as keyof MessageDto)}
                 </TableCell>}
             </TableRow>
           }
