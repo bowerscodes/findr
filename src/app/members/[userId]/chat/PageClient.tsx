925e121827d88c1433a0503e1cca777c554ba764
James Bowers <jamesbowers123@gmail.com>
2025-07-17 16:09:28 +0100

    add functionality to send / receive messages live

--- src/app/members/[userId]/chat/PageClient.tsx
+++ src/app/members/[userId]/chat/PageClient.tsx
@@ -4,31 +4,27 @@ import React from "react";
 import CardInnerWrapper from "@/components/CardInnerWrapper";
 import ChatForm from "./ChatForm";
 import { MessageThread } from "@/types";
-import MessageBox from "./MessageBox";
+import MessageList from "./MessageList";
 
 interface Props {
   messages: MessageThread;
   userId: string;
+  chatId: string;
 }
 
-export default function PageClient({ messages, userId }: Props) {
-    const body = (
-    <div>
-      {messages.length === 0 ? "No messages to display" : (
-        <div>
-          {messages.map(message => (
-            <MessageBox key={message.id} message={message} currentUserId={userId} />
-          ))}
-        </div>
-      )}
-    </div>
-  )
+export default function PageClient({ messages, userId, chatId }: Props) {
 
   return (
     <CardInnerWrapper 
       header="chat"
-      body={body}
+      body={
+        <MessageList 
+          initialMessages={messages} 
+          currentUserId={userId}
+          chatId={chatId} 
+        />
+      }
       footer={<ChatForm />}
     />
   )
-}
+};
--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import PageClient from "./PageClient";
 import { getMessageThread } from "@/app/actions/messageActions";
 import { MessageThread } from "@/types";
+import { createChatId } from "@/lib/util";
+import { getAuthUserId } from "@/app/actions/authActions";
 
 
 export default async function ChatPage({ params }: { params: Promise<{ userId: string }> }) {
-  const { userId } = await params;
-  const messages: MessageThread = await getMessageThread(userId);
+  const currentUserId = await getAuthUserId();
+  const { userId: recipientId } = await params;
+  const messages: MessageThread = await getMessageThread(recipientId);
+  const chatId = createChatId(currentUserId, recipientId)
 
   console.log({ messages });
 
   return (
-    <PageClient messages={messages} userId={userId} />
+    <PageClient messages={messages} userId={currentUserId} chatId={chatId} />
   )
 };
