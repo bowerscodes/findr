74da977eba6556eece389542d7ae19fdd95a2825
James Bowers <jamesbowers123@gmail.com>
2025-07-18 13:34:12 +0100

    add realtime functionality to MessageTable

--- src/hooks/useMessages.tsx
+++ src/hooks/useMessages.tsx
@@ -2,13 +2,33 @@ import { deleteMessage } from "@/app/actions/messageActions";
 import { MessageDto } from "@/types";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-import { useState, useCallback, Key } from "react";
+import { useState, useCallback, Key, useEffect } from "react";
+import useMessageStore from "./useMessageStore";
+import { useShallow } from "zustand/shallow";
 
-export const useMessages = (messages: MessageDto[]) => {
+export const useMessages = (initialMessages: MessageDto[]) => {
+  const { set, remove, messages } = useMessageStore(
+    useShallow(
+      state => ({
+        set: state.set,
+        remove: state.remove,
+        messages: state.messages,
+      })
+    )
+  );
+
   const searchParams = useSearchParams();
   const router = useRouter();
   const isOutbox = searchParams.get("container") === "outbox";
   const [isDeleting, setDeleting] = useState({ id: "", loading: false });
+
+  useEffect(() => {
+    set(initialMessages);
+
+    return () => {
+      set([])
+    };
+  }, [initialMessages, set]);
 
   const columns = [
     { key: isOutbox ? "recipientName" : "senderName", label: isOutbox ? "Recipient" : "Sender" },
@@ -38,6 +58,7 @@ export const useMessages = (messages: MessageDto[]) => {
     columns,
     deleteMessage: handleDeleteMessage,
     selectRow: handleRowSelect,
-    isDeleting
+    isDeleting,
+    messages
   };
 };
--- src/hooks/useNotificationChannel.ts
+++ src/hooks/useNotificationChannel.ts
@@ -1,21 +1,43 @@
+import { useCallback, useEffect, useRef } from "react";
 import { pusherClient } from "@/lib/pusher";
+import { MessageDto } from "@/types";
 import { Channel } from "pusher-js";
-import { useEffect, useRef } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import useMessageStore from "./useMessageStore";
+import { toast } from "react-toastify";
 
 export const useNotificationChannel = (userId: string | null) => {
   const channelRef = useRef<Channel | null>(null);
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const add = useMessageStore(state => state.add);
+  
+  const handleNewMessage = useCallback((message: MessageDto) => {
+    if (pathName === "/messages" && searchParams.get("container") !== "outbox" ) {
+      
+      add(message);
+
+    } else if (pathName !== `/members/${message.senderId}/chat`) {
+
+      toast.info(`New message from ${message.senderName}`)
+
+    }
+  }, [add, pathName, searchParams]);
 
   useEffect(() => {
     if (!userId) return;
     if(!channelRef.current) {
       channelRef.current = pusherClient.subscribe(`private-${userId}`);
+
+      channelRef.current.bind("message:new", handleNewMessage)
     }
 
     return () => {
-      if (channelRef.current) {
+      if (channelRef.current && channelRef.current.subscribed) {
         channelRef.current.unsubscribe();
+        channelRef.current.unbind_all();
         channelRef.current = null;
       }
     }
-  }, [userId])
+  }, [userId, handleNewMessage])
 };
