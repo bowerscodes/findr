6d1505529b343f9c94d56fba661c234c74c802ef
James Bowers <jamesbowers123@gmail.com>
2025-07-21 11:25:43 +0100

    add filters & pagination

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import { getMembers } from "../actions/memberActions";
 import MemberCard from "./MemberCard";
 import { fetchCurrentUserLikeIds } from "../actions/likeActions";
+import PaginationComponent from "@/components/PaginationComponent";
 
 export default async function MembersPage() {
   const members = await getMembers();
   const likeIds = await fetchCurrentUserLikeIds();
 
   return (
-    <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8">
-        {members && members.map(member => (
-          <MemberCard member={member} key={member.id} likeIds={likeIds} />
-        ))}
-    </div>
+    <>
+      <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8">
+          {members && members.map(member => (
+            <MemberCard member={member} key={member.id} likeIds={likeIds} />
+          ))}
+      </div>
+      <PaginationComponent />
+    </>
   );
 }
--- src/components/navbar/TopNavClient.tsx
+++ src/components/navbar/TopNavClient.tsx
@@ -7,6 +7,7 @@ import { TbHeartSearch } from "react-icons/tb";
 import NavLink from './NavLink';
 import UserMenu from './UserMenu';
 import { UserInfo } from '@/types';
+import Filters from './Filters';
 
 interface TopNavClientProps {
   userInfo: UserInfo | undefined
@@ -14,43 +15,46 @@ interface TopNavClientProps {
 
 export default function TopNavClient({ userInfo }: TopNavClientProps) {
   return (
-    <Navbar
-    maxWidth="xl"
-    className="bg-gradient-to-r from-red-400 to-red-600"
-    classNames={{
-      item: [
-        "text-xl",
-        "text-white",
-        "uppercase",
-        "data-[active=true]:text-yellow-200",
-      ]
-    }}
-    >
-      <NavbarBrand as={Link} href="/" className="flex items-center gap-2">
-        <TbHeartSearch size={36} className="text-gray-200"/>
-        <div className="font-bold text-3xl flex">
-          <span className="text-gray-900"><em>findr</em></span>
-        </div>
-      </NavbarBrand>
-      <NavbarContent justify="center">
-        <NavLink href="/members" label="Matches" />
-        <NavLink href="/lists" label="Lists" />
-        <NavLink href="/messages" label="Messages" />
-      </NavbarContent>
-      <NavbarContent justify="end">
-        {userInfo ? (
-          <UserMenu userInfo={userInfo} />
-        ) : (
-          <>
-            <Button as={Link} href="/login" variant="bordered" radius="sm" className="text-white">
-              Login
-            </Button>
-            <Button as={Link} href="/register" variant="bordered" radius="sm" className="text-white">
-              Register
-            </Button>
-          </>
-        )}
-      </NavbarContent>
-    </Navbar>
+    <>
+      <Navbar
+      maxWidth="xl"
+      className="bg-gradient-to-r from-red-400 to-red-600"
+      classNames={{
+        item: [
+          "text-xl",
+          "text-white",
+          "uppercase",
+          "data-[active=true]:text-yellow-200",
+        ]
+      }}
+      >
+        <NavbarBrand as={Link} href="/" className="flex items-center gap-2">
+          <TbHeartSearch size={36} className="text-gray-200"/>
+          <div className="font-bold text-3xl flex">
+            <span className="text-gray-900"><em>findr</em></span>
+          </div>
+        </NavbarBrand>
+        <NavbarContent justify="center">
+          <NavLink href="/members" label="Matches" />
+          <NavLink href="/lists" label="Lists" />
+          <NavLink href="/messages" label="Messages" />
+        </NavbarContent>
+        <NavbarContent justify="end">
+          {userInfo ? (
+            <UserMenu userInfo={userInfo} />
+          ) : (
+            <>
+              <Button as={Link} href="/login" variant="bordered" radius="sm" className="text-white">
+                Login
+              </Button>
+              <Button as={Link} href="/register" variant="bordered" radius="sm" className="text-white">
+                Register
+              </Button>
+            </>
+          )}
+        </NavbarContent>
+      </Navbar>
+      <Filters />
+    </>
   )
 }
