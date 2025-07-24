30bb3335d453710bc654552f79a68a990c23d851
James Bowers <jamesbowers123@gmail.com>
2025-07-24 13:58:33 +0100

    add confirmation modal

--- src/components/AppModal.tsx
+++ src/components/AppModal.tsx
@@ -22,6 +22,12 @@ export default function AppModal ({
       isOpen={isOpen}
       onClose={onClose}
       placement="top-center"
+      motionProps={{
+        variants: {
+          enter: { y:0, opacity: 100, transition: { duration: 0.1 } },
+          exit: { y:100, opacity: 0, transition: { duration: 0.1 } }
+        }
+      }}
     >
       <ModalContent>
         <ModalHeader className="flex flex-col gap-1">{header}</ModalHeader>
